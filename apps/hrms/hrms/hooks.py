cc64f00f02e935d1738ce37dc22ea9f3ed2ca9b5
TejasNikamMphatek <Tejas.Nikam@mphatek.com>
2025-07-21 10:49:23 +0530

    Pushing code for testing on VM = apps/hrms/hrms/hooks.py

--- apps/hrms/hrms/hooks.py
+++ apps/hrms/hrms/hooks.py
@@ -218,6 +218,7 @@ scheduler_events = {
 		"hrms.hr.doctype.daily_work_summary_group.daily_work_summary_group.send_summary",
 		"hrms.hr.doctype.interview.interview.send_daily_feedback_reminder",
 		"hrms.hr.doctype.job_opening.job_opening.close_expired_job_openings",
+        "hrms.hr.doctype.shift_type.shift_type.process_auto_attendance_for_all_shifts"
 	],
 	"daily_long": [
 		"hrms.hr.doctype.leave_ledger_entry.leave_ledger_entry.process_expired_allocation",
