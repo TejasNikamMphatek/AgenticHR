afdd11803df870bdff065f6c8d35d5f94a7f9084
TejasNikamMphatek <Tejas.Nikam@mphatek.com>
2025-08-22 18:58:51 +0530

    changes in project manager dashboard

--- apps/hrms/hrms/hr/page/project_manager_dash/project_manager_dash.js
+++ apps/hrms/hrms/hr/page/project_manager_dash/project_manager_dash.js
@@ -114,11 +114,11 @@ if(frappe.user.has_role("Projects Manager")){
 					// console.log(doc);
 					if (doc.log_type == "IN") {
 						me.checkInLogType = "OUT"
-						frappe.show_alert({ message: __("Sign In Successfully ! "), indicator: "green" });
+						frappe.show_alert({ message: __("Signed In Successfully ! "), indicator: "green" });
 						this.callRefreshData()
 					} else {
 						me.checkInLogType = "IN"
-						frappe.show_alert({ message: __("Sign Out Successfully ! "), indicator: "gray" });
+						frappe.show_alert({ message: __("Signed Out Successfully ! "), indicator: "gray" });
 					}
 
 
