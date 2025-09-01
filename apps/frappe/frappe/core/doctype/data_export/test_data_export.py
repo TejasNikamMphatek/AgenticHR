aa4ec4f7074b144b5ab50375052833ddb6e73fe0
maheshgadalkar <maheshgadalkar2000@gmail.com>
2025-09-01 20:00:56 +0530

    Fix UI Issue Auto Email Select BOX

---
+++ apps/frappe/frappe/core/doctype/data_export/test_data_export.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Pipal Technologies and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestDataExport(FrappeTestCase):
+	pass
---
+++ apps/frappe/frappe/email/doctype/auto_email/auto_email_list.js
@@ -0,0 +1,31 @@
+frappe.listview_settings["Auto Email"] = {
+    onload(listview) {
+        const clean = () => {
+            // Find the Frequency standard filter wrapper
+            const $field = listview.$page
+                .find('.standard-filter-section [data-fieldname="frequency"]');
+
+            if (!$field.length) return;
+
+            // 1) Prefer: revert to native <select>
+            const $select = $field.find('select');
+            if ($select.length && $select.data('selectpicker')) {
+                try { $select.selectpicker('destroy'); } catch (e) {}
+            }
+
+            // 2) Also remove unwanted classes from any leftover wrapper
+            $field.find('div.dropdown.bootstrap-select')
+                .removeClass('dropdown bootstrap-select input-with-feedback form-control input-xs ellipsis');
+        };
+
+        // Run now and after render
+        frappe.after_ajax(clean);
+        setTimeout(clean, 300);
+
+        // Re-run on DOM changes (filters often re-render)
+        const obs = new MutationObserver(() => clean());
+        obs.observe(listview.$page[0], { childList: true, subtree: true });
+        listview.__freqObs = obs;
+    },
+
+};
