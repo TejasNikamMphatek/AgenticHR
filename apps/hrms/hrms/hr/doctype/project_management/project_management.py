e3bfa7fea962b04d88861cc4da4f0d843193d3cd
maheshgadalkar <maheshgadalkar2000@gmail.com>
2025-09-02 16:31:28 +0530

    project management STOP Duplicate Entry && Report list UI Fix

--- apps/hrms/hrms/hr/doctype/project_management/project_management.py
+++ apps/hrms/hrms/hr/doctype/project_management/project_management.py
@@ -1,9 +1,14 @@
 # Copyright (c) 2024, mPHATEK Systems Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class ProjectManagement(Document):
-	pass
+	def before_save(self):
+		seen = set()
+		for member in self.project_team:
+			if member.employee in seen:
+				frappe.throw(f"Duplicate Employee {member.employee} found in Project Team.")
+			seen.add(member.employee)
