47214a5f77fb1506f0aae9676c74a4bd04215a20
maheshgadalkar <maheshgadalkar2000@gmail.com>
2025-09-03 15:21:20 +0530

    Hide unwanted View for project-timesheet-management && Stop Duplicate Entry for same employee in contract Record

--- apps/hrms/hrms/hr/doctype/employee_contract/employee_contract.py
+++ apps/hrms/hrms/hr/doctype/employee_contract/employee_contract.py
@@ -1,9 +1,15 @@
 # Copyright (c) 2024, mPHATEK Systems Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class EmployeeContract(Document):
-	pass
+    def before_save(self):
+        if self.contracts_information:
+            seen = set()
+            for contract_info in self.contracts_information:
+                if contract_info.employee in seen:
+                    frappe.throw(f"Contract already available for employee {contract_info.employee}.")
+                seen.add(contract_info.employee)
