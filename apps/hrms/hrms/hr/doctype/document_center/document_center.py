8bf14ba8defe96d8a6e06d54e4077960ad2172c1
maheshgadalkar <maheshgadalkar2000@gmail.com>
2025-09-04 18:02:28 +0530

    Add Published file Download functionality for employee

--- apps/hrms/hrms/hr/doctype/document_center/document_center.py
+++ apps/hrms/hrms/hr/doctype/document_center/document_center.py
@@ -1,9 +1,21 @@
 # Copyright (c) 2025, mPHATEK Systems Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-
 class DocumentCenter(Document):
-	pass
+    pass
+
+
+@frappe.whitelist(allow_guest=True)
+def force_download(docname):
+    doc = frappe.get_doc("Document Center", docname)
+    if not doc.document:
+        frappe.throw("No document attached!")
+
+    file_doc = frappe.get_doc("File", {"file_url": doc.document})
+
+    frappe.local.response.filename = file_doc.file_name
+    frappe.local.response.filecontent = file_doc.get_content()
+    frappe.local.response.type = "download"
