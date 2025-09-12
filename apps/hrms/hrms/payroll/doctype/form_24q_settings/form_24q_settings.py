e20cebfe2927e044e550e2ceedd84358dc0dfafe
maheshgadalkar <maheshgadalkar2000@gmail.com>
2025-09-12 15:34:59 +0530

    Added New form for FORM 24Q SETTINGS

---
+++ apps/hrms/hrms/payroll/doctype/form_24q_settings/form_24q_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, mPHATEK Systems Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Form24QSettings(Document):
+	pass
