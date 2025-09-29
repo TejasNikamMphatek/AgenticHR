bb8606edb20171014cb96668c61efec3c2d973d4
maheshgadalkar <maheshgadalkar2000@gmail.com>
2025-09-29 14:57:57 +0530

    soved issues : update HRSidebar Links , Improve UI .

--- apps/hrms/hrms/payroll/doctype/salary_structure/salary_structure_list.js
+++ apps/hrms/hrms/payroll/doctype/salary_structure/salary_structure_list.js
@@ -1,7 +1,19 @@
 frappe.listview_settings["Salary Structure"] = {
+	refresh(frm) {
+		$('.form-group[data-fieldname="is_active"] > div.dropdown').removeClass();
+		$('.form-group[data-fieldname="is_active"] button.dropdown-toggle').remove();
+		$('.form-group[data-fieldname="is_active"] > div').removeAttr('title data-original-title');
+	},
+
 	onload: function (list_view) {
 		list_view.page.add_inner_button(__("Bulk Salary Structure Assignment"), function () {
 			frappe.set_route("Form", "Bulk Salary Structure Assignment");
 		});
+
+		setTimeout(function () {
+			$('.form-group[data-fieldname="is_active"] > div.dropdown').removeClass();
+			$('.form-group[data-fieldname="is_active"] button.dropdown-toggle').remove();
+			$('.form-group[data-fieldname="is_active"] > div').removeAttr('title data-original-title');
+		}, 500);
 	},
 };
